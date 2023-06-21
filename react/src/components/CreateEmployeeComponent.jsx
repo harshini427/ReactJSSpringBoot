f4127b6aaa744ab691095a951d6c0fd7ec612db0
harshini427 <77102071+harshini427@users.noreply.github.com>
2023-06-21 16:00:18 +0530

    Update CreateEmployeeComponent.jsx

--- react/src/components/CreateEmployeeComponent.jsx
+++ react/src/components/CreateEmployeeComponent.jsx
@@ -1,123 +1,109 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import EmployeeService from '../services/EmployeeService';
-import "./CreateEmployeeComponent.css";
+import './CreateEmployeeComponent.css';
 
-class CreateEmployeeComponent extends Component {
-  constructor(props) {
-    super(props);
+const CreateEmployeeComponent = (props) => {
+  const [id] = useState(props.match.params.id || '_add');
+  const [name, setName] = useState('');
+  const [department, setDepartment] = useState('');
+  const [sex, setSex] = useState('');
+  const [salary, setSalary] = useState('');
+  const [dob, setDob] = useState('');
+  const [image, setImage] = useState('');
+  const [errors, setErrors] = useState({});
 
-    this.state = {
-      id: this.props.match.params.id || '_add',
-      name: '',
-      department: '',
-      sex: '',
-      salary: '',
-      dob: '',
-      image: ''
-    };
-
-    this.changenameHandler = this.changenameHandler.bind(this);
-    this.changedepartmentHandler = this.changedepartmentHandler.bind(this);
-    this.changesexHandler = this.changesexHandler.bind(this);
-    this.changesalaryHandler = this.changesalaryHandler.bind(this);
-    this.changedobHandler = this.changedobHandler.bind(this);
-    this.saveOrUpdateEmployee = this.saveOrUpdateEmployee.bind(this);
-    this.cancel = this.cancel.bind(this);
-  }
-
-  componentDidMount() {
-    if (this.state.id !== '_add') {
-      EmployeeService.getEmployeeById(this.state.id).then((res) => {
-        let employee = res.data;
-        this.setState({
-          name: employee.name,
-          department: employee.department,
-          sex: employee.sex,
-          salary: employee.salary,
-          dob: employee.dob
-        });
-      });
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    clearValidationError(name);
+    
+    if (name === 'sex') {
+      setSex(value);
+    } else {
+      eval(`set${name.charAt(0).toUpperCase() + name.slice(1)}`)(value);
     }
-  }
+  };
 
-  saveOrUpdateEmployee(e) {
+  const clearValidationError = (fieldName) => {
+    setErrors((prevErrors) => ({ ...prevErrors, [fieldName]: '' }));
+  };
+
+  const saveOrUpdateEmployee = (e) => {
     e.preventDefault();
-    let employee = {
-      name: this.state.name,
-      department: this.state.department,
-      sex: this.state.sex,
-      salary: this.state.salary,
-      dob: this.state.dob,
-      image: this.state.image
-    };
+    const validationErrors = validateForm();
 
-    if (this.state.id === '_add') {
-      EmployeeService.createEmployee(employee).then((res) => {
-        this.props.history.push('/employees');
-      });
+    if (Object.keys(validationErrors).length === 0) {
+      const employee = {
+        name,
+        department,
+        sex,
+        salary,
+        dob,
+        image
+      };
+
+      if (id === '_add') {
+        EmployeeService.createEmployee(employee).then((res) => {
+          props.history.push('/employees');
+        });
+      } else {
+        EmployeeService.saveOrUpdateEmployee(employee, id).then((res) => {
+          props.history.push('/employees');
+        });
+      }
     } else {
-      EmployeeService.saveOrUpdateEmployee(employee, this.state.id).then((res) => {
-        this.props.history.push('/employees');
-      });
+      setErrors(validationErrors);
     }
-  }
+  };
 
-  changenameHandler(event) {
-    this.setState({ name: event.target.value });
-  }
+  const validateForm = () => {
+    const errors = {};
 
-  changedepartmentHandler(event) {
-    this.setState({ department: event.target.value });
-  }
-
-  changesexHandler(event) {
-    this.setState({ sex: event.target.value });
-  }
-
-  changesalaryHandler(event) {
-    this.setState({ salary: event.target.value });
-  }
-
-  changedobHandler(event) {
-    this.setState({ dob: event.target.value });
-  }
-
-  cancel() {
-    this.props.history.push('/employees');
-  }
-
-  getTitle() {
-    if (this.state.id === '_add') {
-      return <h3 className="text-center">Employee Details</h3>;
-    } else {
-      return <h3 className="text-center">Update Employee</h3>;
+    if (name.trim() === '') {
+      errors.name = 'Name is required.';
     }
-  }
 
-  handleImageChange = (e) => {
+    if (department.trim() === '') {
+      errors.department = 'Department is required.';
+    }
+
+    if (sex.trim() === '') {
+      errors.sex = 'Sex is required.';
+    }
+
+    if (salary.trim() === '') {
+      errors.salary = 'Salary is required.';
+    }
+
+    if (dob.trim() === '') {
+      errors.dob = 'Date of Birth is required.';
+    }
+
+    return errors;
+  };
+  const handleImageChange = (e) => {
     const file = e.target.files[0];
     const reader = new FileReader();
-
+  
     reader.onload = (event) => {
-      // this.setState({ base64String : event.target.result.split(",")[1]});
       const base64String = event.target.result.split(",")[1];
-      this.setState({ image: base64String });
-
+      setImage(base64String);
     };
-
+  
     reader.onerror = (error) => {
       console.log("Error: ", error);
     };
-
+  
     if (file) {
       reader.readAsDataURL(file);
     }
   };
-  renderUserImage = () => {
-    if (this.state.image) {
+  
+
+  const renderUserImage = () => {
+    if (image) {
       return (
         <img
-          src={`data:image/jpeg;base64,${this.state.image}`}
+          src={`data:image/jpeg;base64,${image}`}
           alt="User"
           style={{ height: 70, width: 70, borderRadius: 50 }}
           className="user-image"
@@ -125,125 +111,124 @@ class CreateEmployeeComponent extends Component {
       );
     }
     return null;
-  }
+  };
 
-  render() {
-    return (
-      <div>
-        <br />
-        <div className="container">
-          <div className="row">
-            <div className="card col-md-6 offset-md-3 offset-md-3">
-              {this.getTitle()}
-              <div className="card-body">
-                <form>
-                  <div className="form-group">
-                    <label>Name</label>
-                    <input
-                      placeholder="Full Name"
-                      name="name"
-                      className="form-control"
-                      value={this.state.name}
-                      onChange={this.changenameHandler}
-                    />
-                  </div>
-                  <div className="form-group">
-                    <label>Department</label>
-                    <select
-                  placeholder="Department"
-                  name="department"
-                  className="form-control"
-                  value={this.state.department}
-                  onChange={this.changedepartmentHandler}
-                  required>
-                  <option>None</option>
-                  <option>Sales</option>
-                  <option>HR</option>
-                  <option>Manager</option>
-                  <option>Accounts</option>
-                </select>
-                    {/* <input
-                      placeholder="Department"
-                      name="department"
-                      className="form-control"
-                      value={this.state.department}
-                      onChange={this.changedepartmentHandler}
-                    /> */}
-                  </div>
-                  <div className="form-group">
-                    <label>Sex</label>
-                    <div>
+  const getTitle = () => {
+    if (id === '_add') {
+      return <h3 className="text-center">Employee Details</h3>;
+    } else {
+      return <h3 className="text-center">Update Employee</h3>;
+    }
+  };
+
+  return (
+    <div>
+      <br />
+      <div className="container">
+        <div className="row">
+          <div className="card col-md-6 offset-md-3 offset-md-3">
+            {getTitle()}
+            <div className="card-body">
+              <form>
+                <div className="form-group">
+                  <label>Name</label>
+                  <input
+                    placeholder="Full Name"
+                    name="name"
+                    className={`form-control ${errors.name ? 'is-invalid' : ''}`}
+                    value={name}
+                    onChange={handleInputChange}
+                    required
+                  />
+                  {errors.name && <div className="invalid-feedback">{errors.name}</div>}
+                </div>
+                <div className="form-group">
+                  <label>Department</label>
+                  <select
+                    placeholder="Department"
+                    name="department"
+                    className={`form-control ${errors.department ? 'is-invalid' : ''}`}
+                    value={department}
+                    onChange={handleInputChange}
+                    required
+                  >
+                    <option>None</option>
+                    <option>Sales</option>
+                    <option>HR</option>
+                    <option>Manager</option>
+                    <option>Accounts</option>
+                  </select>
+                  {errors.department && <div className="invalid-feedback">{errors.department}</div>}
+                </div>
+                <div className="form-group">
+                  <label>Sex</label>
+                  <div>
                     <label>
-                    <input
-                      type="radio"
-                      name="sex"
-                      value="Male"
-                      checked={this.state.sex === 'Male'}
-                      onChange={(e) => this.setState({ sex: e.target.value })}
-                      required
-                    />{' '}
-                    Male
-                  </label>
-                  <label style={{ marginLeft: '30px' }}>
-                    <input
-                      type="radio"
-                      name="sex"
-                      value="Female"
-                      checked={this.state.sex === 'Female'}
-                      onChange={(e) => this.setState({ sex: e.target.value })}
-                    />{' '}
-                    Female
-                  </label>
-                    </div>
-                    {/* <input
-                      placeholder="Sex"
-                      name="sex"
-                      className="form-control"
-                      value={this.state.sex}
-                      onChange={this.changesexHandler}
-                    /> */}
+                      <input
+                        type="radio"
+                        name="sex"
+                        value="Male"
+                        checked={sex === 'Male'}
+                        onChange={handleInputChange}
+                        required
+                      />{' '}
+                      Male
+                    </label>
+                    <label style={{ marginLeft: '30px' }}>
+                      <input
+                        type="radio"
+                        name="sex"
+                        value="Female"
+                        checked={sex === 'Female'}
+                        onChange={handleInputChange}
+                      />{' '}
+                      Female
+                    </label>
                   </div>
-                  <div className="form-group">
-                    <label>Salary</label>
-                    <input
-                      placeholder="Salary"
-                      name="salary"
-                      className="form-control"
-                      value={this.state.salary}
-                      onChange={this.changesalaryHandler}
-                    />
-                  </div>
-                  <div className="form-group">
-                    <label>Date of Birth</label>
-                    <input
-                      placeholder="Date of Birth"
-                      name="dob"
-                      type='date'
-                      className="form-control"
-                      value={this.state.dob}
-                      onChange={this.changedobHandler}
-                    />
-                  </div>
-                  <div className="form-group" >
-                    <label>Image</label>
-                    <input type="file" accept="image/*" onChange={this.handleImageChange} className="form-control" required />
-                    {this.renderUserImage()}
-                    <small className="form-text ">Upload a profile picture for the user.</small>
-                  </div>
-                  <button className="btn btn-success" onClick={this.saveOrUpdateEmployee}>
-                    Save
-                  </button>
-                  <button className="btn btn-danger" onClick={this.cancel} style={{ marginLeft: '10px' }}>
-                    Cancel
-                  </button>
-                </form>
-              </div>
+                  {errors.sex && <div className="invalid-feedback">{errors.sex}</div>}
+                </div>
+                <div className="form-group">
+                  <label>Salary</label>
+                  <input
+                    placeholder="Salary"
+                    name="salary"
+                    className={`form-control ${errors.salary ? 'is-invalid' : ''}`}
+                    value={salary}
+                    onChange={handleInputChange}
+                  />
+                  {errors.salary && <div className="invalid-feedback">{errors.salary}</div>}
+                </div>
+                <div className="form-group">
+                  <label>Date of Birth</label>
+                  <input
+                    placeholder="Date of Birth"
+                    name="dob"
+                    type="date"
+                    className={`form-control ${errors.dob ? 'is-invalid' : ''}`}
+                    value={dob}
+                    onChange={handleInputChange}
+                  />
+                  {errors.dob && <div className="invalid-feedback">{errors.dob}</div>}
+                </div>
+                <div className="form-group">
+                  <label>Image</label>
+                  <input type="file" accept="image/*" onChange={handleImageChange} className="form-control" required />
+                  {renderUserImage()}
+                  <small className="form-text ">Upload a profile picture for the user.</small>
+                </div>
+                <button className="btn btn-success" onClick={saveOrUpdateEmployee}>
+                  Save
+                </button>
+                <button className="btn btn-danger" onClick={() => props.history.push('/employees')} style={{ marginLeft: '10px' }}>
+                  Cancel
+                </button>
+              </form>
             </div>
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default CreateEmployeeComponent;
